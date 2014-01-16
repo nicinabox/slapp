5a073246849190fcc35e0c1a2592357c9bfeddfd
Nic Aitch <nic@nicinabox.com>
2014-01-16 16:47:06 -0600

    Parse package parts

--- lib/parser.rb
+++ lib/parser.rb
@@ -1,32 +1,103 @@
 class Parser
-  attr_accessor :file, :lines
+  attr_accessor :lines, :contents
 
   def initialize(path)
-    @file = File.read(path)
     @lines = File.readlines(path)
+    @contents = @lines.join('')
   end
 
   def total_size_uncompressed
-    line = lines.select { |line| line =~ total_size('uncompressed') }
-    match = total_size('uncompressed').match(line.first)
+    line = lines.select { |line| line =~ total_size_regex('uncompressed') }
+    match = total_size_regex('uncompressed').match(line.first)
     convert_to_bytes match[1]
   end
 
   def total_size_compressed
-    line = lines.select { |line| line =~ total_size('compressed') }
-    match = total_size('compressed').match(line.first)
+    line = lines.select { |line| line =~ total_size_regex('compressed') }
+    match = total_size_regex('compressed').match(line.first)
     convert_to_bytes match[1]
+  end
+
+  def packages
+    raw_packages.map { |pkg|
+      package = name_parts(pkg.first)
+
+      location = location_regex.match(pkg.first)
+      size = pkg.first.scan(size_regex).flatten
+      description = description_regex.match(pkg.first)
+
+      desc = description[1].gsub(/^#{package[:name]}:/, "")
+      summary = desc.slice(/^(.+)\n/).strip
+
+      raw_desc = desc.gsub(/\n\n /, "\n\n").gsub(/\n /, "\n").strip
+
+      if package[:name] == 'aalib'
+        parsed_desc = ''
+      else
+        parsed_desc = desc.gsub(/(?<!\n)\n /, " ").gsub(/\n\n\s/, "\n\n").strip
+      end
+
+      package.merge({
+        size: {
+          compressed: convert_to_bytes(size[0]),
+          uncompressed: convert_to_bytes(size[1])
+        },
+        location: location[1],
+        path: File.join(location[1], package[:filename]),
+        description: {
+          raw: raw_desc,
+          parsed: parsed_desc
+        },
+        summary: summary
+      })
+    }
   end
 
 private
 
-  def total_size(type)
+  def name_parts(pkg)
+    name = name_regex.match(pkg)
+
+    {
+      filename: name[1],
+      name: name[2],
+      version: name[3],
+      build: name[4],
+      arch: name[5]
+    }
+  end
+
+  def raw_packages
+    contents.scan(package_regex)
+  end
+
+  def package_regex
+    /(PACKAGE NAME.+?)\n\n/m
+  end
+
+  def name_regex
+    /PACKAGE NAME:\s+((.+)-(.+)-(.+)-(.+)\.t\wz)/
+  end
+
+  def location_regex
+    /PACKAGE LOCATION:\s+\.([\S]+)/
+  end
+
+  def size_regex
+    /PACKAGE SIZE\s+\(\w+\):\s+(.+)/
+  end
+
+  def description_regex
+    /PACKAGE DESCRIPTION:\s+(.+)/m
+  end
+
+  def total_size_regex(type)
     /Total size.+\(#{type}\):\s+(.+)$/
   end
 
   def convert_to_bytes(string)
     units = {
-      :'KB' => 1024,
+      :'K' => 1024,
       :'MB' => 1024 * 1024
     }
 
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -15,25 +15,83 @@ describe Parser do
 
   describe '.read' do
     it 'reads the file' do
-      @parser.file.should_not be_nil
+      @parser.lines.should_not be_empty
     end
   end
 
-  describe '#raw' do
-    it "parses the name"
-    it "parses the location"
-    it "parses the size"
-    it "parses the description"
+  describe '#packages' do
+    before do
+      @package = @parser.packages.first
+    end
+
+    it "parses the filename" do
+      @package[:filename].should == 'ConsoleKit-0.4.3-i486-1.txz'
+    end
+
+    it "parses the name" do
+      @package[:name].should == 'ConsoleKit'
+    end
+
+    it "parses the version" do
+      @package[:version].should == '0.4.3'
+    end
+
+    it "parses the location" do
+      @package[:location].should == '/slackware/l'
+    end
+
+    it "parses the path" do
+      @package[:path].should == '/slackware/l/ConsoleKit-0.4.3-i486-1.txz'
+    end
+
+    it "parses the size" do
+      @package[:size].should == {
+        :compressed => 131072,
+        :uncompressed => 624640
+      }
+    end
+
+    it "parses the description" do
+      desc = <<-desc
+ConsoleKit (user, login, and seat tracking framework)
+
+ConsoleKit is a framework for defining and tracking users, login
+sessions, and seats.
+
+Homepage: http://freedesktop.org/wiki/Software/ConsoleKit
+      desc
+
+      parsed_desc = <<-desc
+ConsoleKit (user, login, and seat tracking framework)
+
+ConsoleKit is a framework for defining and tracking users, login sessions, and seats.
+
+Homepage: http://freedesktop.org/wiki/Software/ConsoleKit
+      desc
+
+      @package[:description][:raw].should == desc.strip
+      @package[:description][:parsed].should == parsed_desc.strip
+    end
+
+    it "parses the summary" do
+      @package[:summary].should == "ConsoleKit (user, login, and seat tracking framework)"
+    end
   end
 
-  it "returns the name"
-  it "returns the version"
-  it "returns the arch"
-  it "returns the build"
-  it "returns the path"
-  it "returns the uncompressed size, in bytes"
-  it "returns the compressed size, in bytes"
-  it "returns the description"
-  it "returns the one line summary"
+  it "parses the aalib description" do
+    desc = <<-desc
+aalib (ASCII Art library)
+
+AA-lib is an ASCII art graphics
+library.  Internally, the AA-lib
+API is similar to other graphics
+libraries, but it renders the
+the output into ASCII art (like
+the example to the right :^)
+The developers of AA-lib are
+Jan Hubicka, Thomas A. K. Kjaer,
+Tim Newsome, and Kamil Toman.
+    desc
+  end
 
 end
